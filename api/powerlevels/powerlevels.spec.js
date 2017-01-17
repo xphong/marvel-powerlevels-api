289ad21d801707e074f67291f5e2f97f1eb79f74
Phong Huynh <xphong@gmail.com>
2017-01-17 16:01:27 -0500

    Update tests

--- api/powerlevels/powerlevels.spec.js
+++ api/powerlevels/powerlevels.spec.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 
-const app = require('../../server');
+const app = require('../../app');
 
 describe('PowerLevels', () => {
 
