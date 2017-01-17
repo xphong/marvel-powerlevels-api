f47fe2f2e706a80d30dd38e844cfb4374455e794
Phong Huynh <xphong@gmail.com>
2017-01-16 22:16:15 -0500

    Mongoose connection

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(config.database);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB.');
+});
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
